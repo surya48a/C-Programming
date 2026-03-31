42a23bf3733540cf053614f414862b7e7d3b9fd9
Surya <suryabot48@gmail.com>
2026-03-31 17:19:35 +0530

    Delete datatype directory

--- datatype/p5.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main()
-{
-unsigned char a=250;
-unsigned char b=10;
-unsigned char c=a+b;
-printf("%d\n",c);
-printf("%x\n",c);
-}
--- datatype/p7.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-void main()
-{
-char c=0x80;
-printf("%d\n",c);
-printf("%x\n",c);
-}
